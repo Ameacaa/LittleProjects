dfdd40ff96dbb4ff31b6e5e7a1c9c723f978613d
Ameacaa <7kevinla@gmail.com>
2022-10-17 13:05:17 +0100

    Math curiosity 3x+1 implemented and Work

--- MatematicalCuriosities/_3x1.cs
+++ MatematicalCuriosities/_3x1.cs
@@ -2,5 +2,107 @@
 {
     class _3x1
     {
+        private static ulong InputNumber(string message)
+        {
+            string? inputString;
+            ulong inputNumber;
+            do
+            {
+                Console.Write(message);
+                inputString = Console.ReadLine().Trim();
+                if (string.IsNullOrEmpty(inputString)) { Console.WriteLine("ERROR: Empty value"); continue; }
+            } while (!ulong.TryParse(inputString, out inputNumber));
+            return inputNumber;
+        }
+
+        private static void NormalMode(ulong min, ulong max)
+        {
+            for (ulong v = min; v < max; v++)
+            {
+                int steps = 0;
+                ulong t = v;
+                DateTime start = DateTime.Now;
+                Console.Write("[" + t + "] - ");
+                while (t != 1)
+                {
+                    steps++;
+                    if (t % 2 == 0) { t /= 2; }
+                    else { t = 3 * t + 1; }
+                }
+                Console.Write(steps + " - ");
+                Console.Write("(" + (DateTime.Now - start) + ")");
+                Console.WriteLine();
+            }
+        }
+
+        private static void DetailMode(ulong min, ulong max)
+        {
+            for (ulong v = min; v < max; v++)
+            {
+                int steps = 0;
+                ulong t = v;
+                DateTime start = DateTime.Now;
+                Console.Write("[" + t + "] - ");
+                while (t != 1)
+                {
+                    steps++;
+                    if (t % 2 == 0) { t /= 2; }
+                    else { t = 3 * t + 1; }
+                    Console.Write(t + " - ");
+                }
+                Console.WriteLine(steps);
+                Console.WriteLine  ("(" + (DateTime.Now - start) + ")");
+                Console.WriteLine();
+            }
+        }
+
+        public static void Menu()
+        {
+            ulong min = 1, max = 100;
+            Console.WriteLine("\n"
+                + "***********************************************************************\n"
+                + "****** Math 3x+1                                       by Ameaca ******\n"
+                + "***********************************************************************\n"
+                + "**                                                                   **\n"
+                + "**   1 --- Change Min value                                          **\n"
+                + "**   2 --- Change Max value                                          **\n"
+                + "**   3 --- Launch Normal mode                                        **\n"
+                + "**   4 --- Launch Detail mode                                        **\n"
+                + "**   0 --- Return to Main Menu                                       **\n"
+                + "**                                                                   **\n"
+                + "***********************************************************************");
+            do
+            {
+                Console.Write("Menu Choose: ");
+                ConsoleKey inputMenu = Console.ReadKey().Key;
+                Console.WriteLine();
+                switch (inputMenu)
+                {
+                    case ConsoleKey.NumPad0:
+                        Console.WriteLine("Returning to Main Menu");
+                        return;
+                    case ConsoleKey.NumPad1:
+                        min = InputNumber("Enter a min value: ");
+                        break;
+                    case ConsoleKey.NumPad2:
+                        max = InputNumber("Enter a Max value: ");
+                        break;
+                    case ConsoleKey.NumPad3:
+                        NormalMode(min, max);
+                        break;
+                    case ConsoleKey.NumPad4:
+                        DetailMode(min, max);
+                        break;
+                    default:
+                        Console.WriteLine("ERROR INPUT: Select a valid number");
+                        break;
+                }
+            } while (true);
+
+
+            
+            
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,39 +1,36 @@
 ﻿using LittleProjects.VigenereEncoder;
+using LittleProjects.MatematicalCuriosities;
 
-namespace LittleProjects
-{
-    class Program
-    { 
-        static void Main()
-        {
-            Console.WriteLine("\n"
+Console.Clear();
+Console.WriteLine("\n"
                 + "***********************************************************************\n"
                 + "****** Main Menu                                       by Ameaca ******\n"
                 + "***********************************************************************\n"
                 + "**                                                                   **\n"
                 + "**   1 --- Vigenere Encoder                                          **\n"
+                + "**   2 --- Math Curiosity (3x+1)                                     **\n"
+                + "**   3 --- Math Curiosity (6174)                                     **\n"
                 + "**   0 --- Quit                                                      **\n"
                 + "**                                                                   **\n"
                 + "***********************************************************************");
-            do
-            {
-                Console.Write("Menu Choose: ");
-                ConsoleKey inputMenu = Console.ReadKey().Key;
-                Console.WriteLine();
-                switch (inputMenu)
-                {
-                    case ConsoleKey.NumPad0:
-                        Console.WriteLine("Bye-bye");
-                        return;
-                    case ConsoleKey.NumPad1:
-                        VigenereEncoder.VigenereEncoder.Menu();
-                        break;
-
-                    default:
-                        Console.WriteLine("ERROR INPUT: Select a valid number");
-                        break;
-                }
-            } while (true);
-        }
+do
+{
+    Console.Write("Menu Choose: ");
+    ConsoleKey inputMenu = Console.ReadKey().Key;
+    Console.WriteLine();
+    switch (inputMenu)
+    {
+        case ConsoleKey.NumPad0:
+            Console.WriteLine("Bye-bye");
+            return;
+        case ConsoleKey.NumPad1:
+            VigenereEncoder.Menu();
+            break;
+        case ConsoleKey.NumPad2:
+            _3x1.Menu();
+            break;
+        default:
+            Console.WriteLine("ERROR INPUT: Select a valid number");
+            break;
     }
-}
+} while (true);
